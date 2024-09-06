2494bc2d29993aade1c5bc6061d883b3134edf4f
Tyron <lucena.tyronscott@gmail.com>
2024-09-06 22:47:41 +0800

    removed backend stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,7 @@
 import { defineConfig } from 'vite'
 
-import type { Plugin } from 'vite'
-import type { Adapter } from 'vite-plugin-mix'
-import mixPlugin from 'vite-plugin-mix'
-
-interface MixConfig {
-  handler: string
-  adapter?: Adapter | undefined
-}
-
-type MixPlugin = (config: MixConfig) => Plugin
-
-interface Mix {
-  default: MixPlugin
-}
-
-const mix = (mixPlugin as unknown as Mix).default
-
-// use mix as function
-
 export default defineConfig({
   plugins: [
-    mix({
-      handler: './src/api/api.ts',
-    }),
+
   ],
 })
